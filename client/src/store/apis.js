eba52a3005477c60d763871fa75b68721c68be0b
Harsh Agarwal <haragarw@publicisgroupe.net>
2019-03-20 12:19:52 +0530

    Proxy url changed

--- client/src/store/apis.js
+++ client/src/store/apis.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 import { apiKey } from './constants';
 
 export function getBooksList(searchString) {
-    return axios.get(`http://localhost:3001/api/getBooksList/${searchString}/${apiKey}`)
+    return axios.get(`https://goodreads-search-app.herokuapp.com/api/getBooksList/${searchString}/${apiKey}`)
         .then(res => Promise.resolve(res.data));
 }
 
 export function getBookDetail(bookId) {
-    return axios.get(`http://localhost:3001/api/getBookDetail/${bookId}/${apiKey}`)
+    return axios.get(`https://goodreads-search-app.herokuapp.com/api/getBookDetail/${bookId}/${apiKey}`)
         .then(res => Promise.resolve(res.data));
 }
